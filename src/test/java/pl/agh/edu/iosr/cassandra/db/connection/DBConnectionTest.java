426e2e617671632baec68ba3ad64787084d3f116
Krzysztof Nawrot <krzys.nawrot@gmail.com>
2015-05-19 16:23:53 +0200

    Created dummy tests.

---
+++ src/test/java/pl/agh/edu/iosr/cassandra/db/connection/DBConnectionTest.java
@@ -0,0 +1,16 @@
+package pl.agh.edu.iosr.cassandra.db.connection;
+
+import junit.framework.TestCase;
+
+public class DBConnectionTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+	
+	public void shouldConnect() {
+		//TODO: implement me!
+		fail("Not yet implemented");
+	}
+
+}
