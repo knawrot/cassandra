5d4442ff3ea86c2c38ff61b2be2b74f839696e1e
Krzysztof Nawrot <krzys.nawrot@gmail.com>
2015-05-26 10:57:20 +0200

    Basic tests implemented.

--- src/test/java/pl/agh/edu/iosr/cassandra/db/connection/DBConnectionTest.java
+++ src/test/java/pl/agh/edu/iosr/cassandra/db/connection/DBConnectionTest.java
@@ -1,16 +1,25 @@
 package pl.agh.edu.iosr.cassandra.db.connection;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+
 import junit.framework.TestCase;
 
 public class DBConnectionTest extends TestCase {
-
-	protected void setUp() throws Exception {
-		super.setUp();
-	}
 	
 	public void shouldConnect() {
-		//TODO: implement me!
-		fail("Not yet implemented");
+		Cluster cluster = Cluster.builder()
+			      .addContactPoint(DBConnectionProperties.NODE)
+			      .withPort(DBConnectionProperties.PORT)
+			      .build();
+		Session session = cluster.connect(DBConnectionProperties.KEYSAPCE);
+		
+		assertTrue(session != null);
+		try {
+			session.execute("USE " + DBConnectionProperties.KEYSAPCE + ";");
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
 	}
 
 }
